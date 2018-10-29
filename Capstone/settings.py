9cf1e74fe7e8bb5041b484d73a49e5b0bd986b2b
ventusxu <xuzhixuanventus@gmail.com>
2018-10-28 20:51:13 -0700

    configure for heroku deployment

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -136,3 +136,6 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.twitter.TwitterOAuth',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
