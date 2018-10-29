3ddefbf3cea5bcb55f2ee2617e5c7093f3639ddc
ventusxu <xuzhixuanventus@gmail.com>
2018-10-29 02:30:16 -0700

    added whitelist email domain for 'gmail.com' and 'uci.edu'.

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Social Auth Django Settings
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.open_id.OpenIdAuth',
@@ -147,6 +148,8 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = ['gmail.com', 'uci.edu']
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1011968808749-i3u3hj0la6nnosvcs40cc3spmlvs3kg4.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '1eJUZwLIdadsvJGy24iiiF2u'
